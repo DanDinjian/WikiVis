4b4877de4f6d5aa5143c8569f28196a29bf4c041
asharp02 <adrian.sharpe@tufts.edu>
2018-04-08 16:13:59 -0400

    Added wikiviz template

--- flaskserver.py
+++ flaskserver.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/wikiviz')
+def home():
+    return render_template("wikiviz.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
